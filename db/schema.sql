6b7e603ecca5b818a252154786ac2c033f5f940c
Lorreina Guyett <slorreina369@gmail.com>
2022-07-21 14:26:36 -0700

    set up votes table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -19,4 +21,13 @@ CREATE TABLE voters(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL
+);
+CREATE TABLE votes(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    voter_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
+    created_id DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_voter UNIQUE (voter_id),
+    CONSTRAINT fk_voter FOREIGN KEY(voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+    CONSTRAINT fk_candidates FOREIGN KEY(candidate_id) REFERENCES candidates(id) on DELETE CASCADE
 );
